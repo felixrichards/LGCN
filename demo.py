1d5a7d65205a2f34aede5deb6de065eb4a677c00
Felix Richards <felixarichards@gmail.com>
2020-12-02 17:54:46 +0000

    Use validation set in demo

--- demo.py
+++ demo.py
@@ -7,13 +7,14 @@ from lgcn.models import LGCN
 
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-train_loader, val_loader, test_loader = mnist(batch_size=512, rotate=True)
+idxs = (torch.arange(50000), torch.arange(50000, 60000))
+train_loader, val_loader, test_loader = mnist(batch_size=512, rotate=True, idxs=idxs)
 
 model = LGCN(1, 10, no_g=4).to(device)
 
 optimizer = optim.Adam(model.parameters())
 scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer)
-train(model, [train_loader, val_loader], epochs=10, opt=optimizer,
+train(model, [train_loader, val_loader], epochs=1, opt=optimizer,
       device=device, sch=scheduler)
 
 evaluate(model, test_loader, device=device)
